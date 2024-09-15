cf443dabfeb27744a4073c8fb23166818014ca47
Shaun Cains <shaun.cains@gmail.com>
2024-09-15 21:54:14 +0100

    chore:yac

--- src/main.rs
+++ src/main.rs
@@ -63,7 +63,7 @@ async fn main() -> Result<()> {
     // Initialize the starting timestamp (now - time_period)
     let mut current_timestamp = Utc::now().naive_utc();
 
-    // Initialize the results_map with no_of_periods intervals and 5 price holders
+    // Initialize a results_map with a 5 price vector
     let mut results_map: HashMap<
         NaiveDateTime,
         (
@@ -75,57 +75,86 @@ async fn main() -> Result<()> {
         ),
     > = HashMap::new();
 
-    // Fill in initial timestamps
+    // Fill in initial timestamps and create
+    // the empty placeholders for the volatility calculation
     for _ in 0..no_of_periods {
         results_map.insert(current_timestamp, (None, None, None, None, None));
         current_timestamp = current_timestamp - time_duration;
     }
 
-    println!("Get and Insert Polygon data.. ");
-    let polygon_data = get_polygon_data(&time_period, no_of_periods.try_into().unwrap()).await?;
-    for (timestamp, vw) in polygon_data {
-        // Round the timestamp according to the time_period
-        let rounded_timestamp = round_to_period(timestamp, &time_period);
-        // Insert or update the rounded timestamp in the HashMap
-        results_map
-            .entry(rounded_timestamp)
-            .and_modify(|e| e.0 = Some(vw))
-            .or_insert((Some(vw), None, None, None, None)); // Set Polygon, others remain None
-    }
-
-    println!("Get and Insert Dune data..");
-    let on_chain_prices = fetch_dune_data(&time_period, no_of_periods.try_into().unwrap()).await?;
-    for (day_str, price) in on_chain_prices {
-        let day_str = day_str.trim();
-        if let Ok(date_time) = NaiveDateTime::parse_from_str(&day_str, "%Y-%m-%d %H:%M:%S%.f %Z") {
-            let rounded_timestamp = round_to_period(date_time, &time_period);
-            results_map
-                .entry(rounded_timestamp)
-                .and_modify(|e| e.1 = Some(price))
-                .or_insert((None, Some(price), None, None, None)); // Set Dune price, others remain None
-        } else {
-            println!("Failed to parse date: {}", day_str);
+    // Attempt to fetch and insert Polygon data
+    println!("Fetching Polygon data...");
+    match get_polygon_data(&time_period, no_of_periods.try_into().unwrap()).await {
+        Ok(polygon_data) => {
+            for (timestamp, vw) in polygon_data {
+                let rounded_timestamp = round_to_period(timestamp, &time_period);
+                results_map
+                    .entry(rounded_timestamp)
+                    .and_modify(|e| e.0 = Some(vw))
+                    .or_insert((Some(vw), None, None, None, None)); // Set Polygon, others remain None
+            }
+        }
+        Err(e) => {
+            println!("Failed to fetch Polygon data: {}", e);
         }
     }
 
-    println!("Get and Insert BitFinex data..");
-    let coin_api_data = get_coin_api_data(&time_period).await?;
-    for (timestamp, average_price) in coin_api_data {
-        let rounded_timestamp = round_to_period(timestamp, &time_period);
-        results_map
-            .entry(rounded_timestamp)
-            .and_modify(|e| e.3 = Some(average_price))
-            .or_insert((None, None, None, Some(average_price), None)); // Set BitFinex price, others remain None
+    // Attempt to fetch and insert Dune data
+    println!("Fetching Dune data...");
+    match fetch_dune_data(&time_period, no_of_periods.try_into().unwrap()).await {
+        Ok(on_chain_prices) => {
+            for (day_str, price) in on_chain_prices {
+                let day_str = day_str.trim();
+                if let Ok(date_time) =
+                    NaiveDateTime::parse_from_str(&day_str, "%Y-%m-%d %H:%M:%S%.f %Z")
+                {
+                    let rounded_timestamp = round_to_period(date_time, &time_period);
+                    results_map
+                        .entry(rounded_timestamp)
+                        .and_modify(|e| e.1 = Some(price))
+                        .or_insert((None, Some(price), None, None, None)); // Set Dune price, others remain None
+                } else {
+                    println!("Failed to parse date: {}", day_str);
+                }
+            }
+        }
+        Err(e) => {
+            println!("Failed to fetch Dune data: {}", e);
+        }
     }
 
-    println!("Get and Insert Kraken data.. ");
-    let kraken_data = get_kraken_data(&time_period).await?;
-    for (timestamp, average_price) in kraken_data {
-        let rounded_timestamp = round_to_period(timestamp, &time_period);
-        results_map
-            .entry(rounded_timestamp)
-            .and_modify(|e| e.2 = Some(average_price))
-            .or_insert((None, None, Some(average_price), None, None)); // Set Kraken price, others remain None
+    // Attempt to fetch and insert BitFinex data
+    println!("Fetching BitFinex data...");
+    match get_coin_api_data(&time_period).await {
+        Ok(coin_api_data) => {
+            for (timestamp, average_price) in coin_api_data {
+                let rounded_timestamp = round_to_period(timestamp, &time_period);
+                results_map
+                    .entry(rounded_timestamp)
+                    .and_modify(|e| e.3 = Some(average_price))
+                    .or_insert((None, None, None, Some(average_price), None)); // Set BitFinex price, others remain None
+            }
+        }
+        Err(e) => {
+            println!("Failed to fetch BitFinex data: {}", e);
+        }
+    }
+
+    // Attempt to fetch and insert Kraken data
+    println!("Fetching Kraken data...");
+    match get_kraken_data(&time_period).await {
+        Ok(kraken_data) => {
+            for (timestamp, average_price) in kraken_data {
+                let rounded_timestamp = round_to_period(timestamp, &time_period);
+                results_map
+                    .entry(rounded_timestamp)
+                    .and_modify(|e| e.2 = Some(average_price))
+                    .or_insert((None, None, Some(average_price), None, None)); // Set Kraken price, others remain None
+            }
+        }
+        Err(e) => {
+            println!("Failed to fetch Kraken data: {}", e);
+        }
     }
 
     // Calculate and print volatility
